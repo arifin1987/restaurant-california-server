5964d8f83ff9814005f2c2b4ff9fe14097af7aaf
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-08-31 10:59:02 -0700

    created verifyJwt middleware and decodedEmail and set these to carts related api to verify cart data using jwt

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 
-app.use(express.json());
+// middleware
 app.use(cors());
+app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.59h68ks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -90,10 +111,18 @@ async function run() {
       const result = await cartCollection.insertOne(item);
       res.send(result);
     });
-    app.get("/carts", async (req, res) => {
+    app.get("/carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
+      }
+
+      // This decodedEmail portion wrote here for jwt and verifyjwt
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
       }
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
